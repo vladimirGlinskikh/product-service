eced7b69da9330719777d5ad2864d24fc6e408a9
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-11-13 21:47:05 +0300

    Created Skeleton Application and MongoDB Database

---
+++ src/test/java/kz/zhelezyaka/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
